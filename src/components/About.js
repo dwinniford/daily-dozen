ef96d85b7f043f7965f495717129e778b20eb636
Dave Winniford <dwinniford@gmail.com>
2020-06-29 15:47:31 -0400

    connect react router and styled components

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Title} from '../style/base.js'
+
+export default function About() {
+    return (
+        <div>
+            <Title>About Daily Dozen</Title>
+        </div>
+    )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Title} from '../style/base.js'
+
+export default function Home() {
+    return (
+        <div>
+            <Title>Daily Dozen Home</Title>
+            
+        </div>
+    )
+}
