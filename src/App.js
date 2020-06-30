86d06de4e1c3121e6a0a51f83df68e939321485d
Dave Winniford <dwinniford@gmail.com>
2020-06-30 15:28:54 -0400

    adds dashboard container

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home  from './components/Home.js'
 import About from './components/About.js'
 import {BackgroundOverlay} from './style/base.js'
 import NavBar from './components/NavBar.js'
+import DashboardContainer from './containers/DashboardContainer'
 
 function App() {
   return (
@@ -15,9 +16,13 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
+          <Route path="/dashboard">
+            <DashboardContainer />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
+
         </Switch>
       </BackgroundOverlay>
     </BrowserRouter>
