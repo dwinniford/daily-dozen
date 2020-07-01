095aa201df46ee2d0f6cc2add9574ad57c974035
Dave Winniford <dwinniford@gmail.com>
2020-07-01 15:18:47 -0400

    category uses hook to toggle types display

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState} from 'react'
 import {BlackButton} from '../style/base.js'
 
+
 export default function Category(props) {
+    const [display, toggleDisplay] = useState(false)
+    const renderTypes = () => {
+        return "Types of food test"
+    }
+    const handleToggle = () => {
+        display ? toggleDisplay(false) : toggleDisplay(true)
+    }
+
     return (
         <div>
-            <BlackButton>{props.category.name}</BlackButton>
+            <BlackButton onClick={handleToggle}>{props.category.name}</BlackButton>
+            {display ? renderTypes() : null}
         </div>
     )
 }
