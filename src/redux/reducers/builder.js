f381b6930eafbdd11dac0674b8b91d14a05caefa
Dave Winniford <dwinniford@gmail.com>
2020-07-06 15:56:05 -0400

    remove ingredient updates counter state

--- src/redux/reducers/builder.js
+++ src/redux/reducers/builder.js
@@ -10,7 +10,7 @@ const initialState = {
 export default function builder(state = initialState, action) {
     switch(action.type) {
         case "ADD_INGREDIENT":
-            let new_categories = state.categories.map(cat => {
+            const categoriesAdd = state.categories.map(cat => {
                 if (cat.types.includes(action.ingredient)) {
                     return {
                         ...cat, servings: {
@@ -23,9 +23,22 @@ export default function builder(state = initialState, action) {
                     return cat
                 }
             })
-            return {...state, categories: new_categories, ingredients: [...state.ingredients, action.ingredient]};
+            return {...state, categories: categoriesAdd, ingredients: [...state.ingredients, action.ingredient]};
         case "REMOVE_INGREDIENT":
-            return {...state, ingredients: state.ingredients.filter(i => i !== action.ingredient)}
+            const categoriesRemove = state.categories.map(cat => {
+                if (cat.types.includes(action.ingredient)) {
+                    return {
+                        ...cat, servings: {
+                            ...cat.servings,
+                            used: cat.servings.used - 1,
+                            quantity: cat.servings.quantity + 1 
+                        }
+                    }
+                } else {
+                    return cat
+                }
+            })
+            return {...state, categories: categoriesRemove, ingredients: state.ingredients.filter(i => i !== action.ingredient)}
         default:
             return state;
     }
