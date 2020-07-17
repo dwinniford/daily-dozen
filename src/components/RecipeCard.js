9cbf95f9b64fb35f270e23f328108ebdf7e34156
Dave Winniford <dwinniford@gmail.com>
2020-07-17 09:19:07 -0500

    add recipe card info

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -14,11 +14,22 @@ export default function RecipeCard(props) {
         return <ListItem key={i}>{i}</ListItem>
         })
     }
+
+    const renderInfoCard = () => {
+        // display image, source with link to recipe, ingredients list
+        return (
+            <>
+                <img src={props.recipe.image} alt={props.recipe.label} />
+                <a href={props.recipe.url}>{props.recipe.source}</a>
+                {renderIngredients()}
+            </>
+        )
+    }
     
     return (
         <div>
             <BlackButton onClick={(event) => handleToggle()} key={props.recipe.label+"-"+props.recipe.source}>{props.recipe.label}</BlackButton>
-            {display? renderIngredients() : null }
+            {display? renderInfoCard() : null }
         </div>
     )
 }
