4e74eb7b504f61f23a763d0505285d5dd7223e11
Dave Winniford <dwinniford@gmail.com>
2020-07-20 16:55:59 -0500

    adds tag form

---
+++ src/components/TagForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TagForm() {
+    return (
+        <form>
+            <label for="name">enter a tag</label>
+            <input type="text" name="tag" ></input>
+            
+        </form> 
+    )
+}
