ab65b067237b8056304fa514df6214f1ccca7d4f
Dave Winniford <dwinniford@gmail.com>
2020-07-21 09:00:43 -0500

    clean up errors

--- src/components/TagForm.js
+++ src/components/TagForm.js
@@ -1,22 +1,21 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {LabelInputForm, FormInput, FormLabel} from '../style/dashboard'
 import {connect} from 'react-redux'
 
 function TagForm(props) {
-    const [tagSearchResults, setTagSearchResults] = useState([])
+    
     
     const handleInputKeyUp = (event) => {
         console.log(event.target.value)
-        // setTagSearchResults([...tagSearchResults, event.target.value])
         props.searchTags(event.target.value)
     }
 
     return (
         <LabelInputForm>
-            <FormLabel for="name">enter a tag</FormLabel>
+            <FormLabel htmlFor="name">enter a tag</FormLabel>
             <FormInput onKeyUp={(event) => handleInputKeyUp(event)} type="text" name="tag" ></FormInput>
             <ul>
-                {props.tagSearchResults.map(t => <li>{t}</li>)}
+                {props.tagSearchResults.map(t => <li key={t}>{t}</li>)}
             </ul>
         </LabelInputForm> 
     )
