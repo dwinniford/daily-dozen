556fd232faa461d6f54d8c4283085fdf437e1295
Dave Winniford <dwinniford@gmail.com>
2020-07-21 08:41:01 -0500

    tag form uses hook to display text in list

--- src/components/TagForm.js
+++ src/components/TagForm.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {LabelInputForm, FormInput, FormLabel} from '../style/dashboard'
 
 export default function TagForm() {
+    const [tagSearchResults, setTagSearchResults] = useState([])
+    
     const handleInputKeyUp = (event) => {
         console.log(event.target.value)
+        setTagSearchResults([...tagSearchResults, event.target.value])
     }
 
     return (
         <LabelInputForm>
             <FormLabel for="name">enter a tag</FormLabel>
             <FormInput onKeyUp={(event) => handleInputKeyUp(event)} type="text" name="tag" ></FormInput>
-            
+            <ul>
+                {tagSearchResults.map(t => <li>{t}</li>)}
+            </ul>
         </LabelInputForm> 
     )
 }
