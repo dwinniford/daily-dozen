b503c8fd246211b9b68fff305f217ac88d633e6d
Dave Winniford <dwinniford@gmail.com>
2020-07-21 08:23:11 -0500

    adds key up event to tag input

--- src/components/TagForm.js
+++ src/components/TagForm.js
@@ -2,11 +2,16 @@ import React from 'react'
 import {LabelInputForm, FormInput, FormLabel} from '../style/dashboard'
 
 export default function TagForm() {
+    const handleInputKeyUp = (event) => {
+        console.log(event.target.value)
+    }
+
     return (
         <LabelInputForm>
             <FormLabel for="name">enter a tag</FormLabel>
-            <FormInput type="text" name="tag" ></FormInput>
+            <FormInput onKeyUp={(event) => handleInputKeyUp(event)} type="text" name="tag" ></FormInput>
             
         </LabelInputForm> 
     )
 }
+
