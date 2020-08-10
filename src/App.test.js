b6cd73fb3c738f8d78b70631ddb78b85dfc04612
Dave Winniford <dwinniford@gmail.com>
2020-08-10 09:30:18 -0400

    adds first getByText test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-// import { render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
 });
+
+it('renders home page title', () => {
+  const {getByText} = render(<App />)
+  expect(getByText('Daily Dozen Home')).toBeInTheDocument();
+})
