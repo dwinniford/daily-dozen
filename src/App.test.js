7447564b14c126d4d13e407961116f30417d05d6
Dave Winniford <dwinniford@gmail.com>
2020-08-11 09:50:08 -0400

    adds before and after actions to app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM, {unmountComponentAtNode} from 'react-dom'
 import { render } from '@testing-library/react';
 import App from './App';
 
+let container = null 
+
+beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container)
+    container.remove()
+    container = null
+})
+
 test('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<App />, container)
 });
 
 it('renders home page title', () => {
-  const {getByText} = render(<App />)
+  const {getByText} = render(<App />, container)
   expect(getByText('Daily Dozen Home')).toBeInTheDocument();
 })
