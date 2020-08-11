d4b93ee2f71115398172a597c825a4e71be40052
Dave Winniford <dwinniford@gmail.com>
2020-08-11 09:45:29 -0400

    adds before and after action to navbar test

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM, {unmountComponentAtNode} from 'react-dom'
 import { render } from '@testing-library/react';
 import NavBar from './NavBar'
 import {BrowserRouter} from 'react-router-dom'
 
+let container = null 
+
+beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container)
+    container.remove()
+    container = null
+})
 test('contains dashboard link', () => {
-    const {getByText} = render(<BrowserRouter><NavBar /></BrowserRouter>)
+    // use render or ReactDOM.render? react docs use render imported from 'react-dom'
+    // ReactDOM.render does not have getByText
+    const {getByText} = render(<BrowserRouter><NavBar /></BrowserRouter>, container)
     expect(getByText('Dashboard')).toBeInTheDocument()
 })
 
