c9c918465ee68d4903adc486f0d986f0950cab1f
dwinniford <dwinniford@gmail.com>
2021-02-23 12:15:13 -0600

    commit all before merge

--- src/components/TypesCard.tsx
+++ src/components/TypesCard.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {TypesGrid, ListItem, AddButton} from '../style/dashboard.js'
 
-type Props = {addIngredient: any, types: any}
+type Props = {addIngredient: Function, types: any}
 export default function TypesCard(props: Props) {
     const handleAdd = (name: string) => {
         props.addIngredient(name)
     }
     
     const renderTypes = () => {
-        return props.types.map( (t: string) => <ListItem key={t} >{t}<AddButton onClick={(event: any) => handleAdd(t)}>+</AddButton></ListItem >)
+        return props.types.map( (t: string) => <ListItem key={t} >{t}<AddButton onClick={(event: object) => handleAdd(t)}>+</AddButton></ListItem >)
     }
     return (
         <TypesGrid>
