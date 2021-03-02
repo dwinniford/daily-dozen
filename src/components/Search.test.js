be1571b405983121769af350d875c0596a894475
David Winniford <dwinniford@gmail.com>
2021-03-02 11:27:22 -0600

    uses before and after functions

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -1,16 +1,37 @@
 import Search from './Search'
 import React from 'react'
+import {unmountComponentAtNode} from 'react-dom'
 import {render} from '@testing-library/react'
-import App from '../App';
+
 
 import { Provider } from 'react-redux'
 import store from '../redux/store'
+
+let container = null 
+let component = null
+let getByText = null
+
+beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+    component = render(<Provider store={store} ><Search /></Provider>, container)
+    getByText = component.getByText
+    // console.log(getByText)
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container)
+    container.remove()
+    component = null
+    getByText = null 
+    container = null
+})
 
 describe('Recipe card expands and collapses', () => {
   
     test('it displays a list of recipes', () => {
    
-        const {getByText} = render(<Provider store={store} ><Search /></Provider> )
+        
     
         expect(getByText('Find a Recipe')).toBeInTheDocument()
         expect(getByText('Black Eyed Peas')).toBeInTheDocument()
