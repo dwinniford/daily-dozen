f26e822589123d7cd2f88f9b89d727cca2c62177
David Winniford <dwinniford@gmail.com>
2021-03-11 17:05:54 -0600

    covert Home to tsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import './App.css';
-import Home  from './components/Home.js'
+import Home  from './components/Home'
 import About from './components/About.js'
 import {BackgroundOverlay} from './style/base.js'
 import NavBar from './components/NavBar'
