9eb9ae3977b87d5d29624c360667fccc6c03b70c
David Winniford <dwinniford@gmail.com>
2021-03-15 13:11:22 -0500

    add to meal plan checks for duplicate

--- src/redux/reducers/mealPlan.js
+++ src/redux/reducers/mealPlan.js
@@ -3,7 +3,12 @@ const initialState = []
 export default function mealPlan(state = initialState, action) {
     switch(action.type) {
         case 'ADD_TO_MEAL_PLAN':
-            return [...state, action.recipe]
+            if(!state.find((recipe) => recipe.url === action.recipe.url)) {
+                return [...state, action.recipe]
+            } else {
+                return state
+            }
+            
         default:
             return state;
     }
