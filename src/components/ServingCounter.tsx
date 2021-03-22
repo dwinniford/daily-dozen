969452c3b000995066979b60b504bb43ccafdffa
David Winniford <dwinniford@gmail.com>
2021-03-22 15:04:07 -0500

    cleanup serving counter

--- src/components/ServingCounter.tsx
+++ src/components/ServingCounter.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react'
+import React from 'react'
 import {EmptyCircle, WhiteCheckCircle} from '../style/icons'
 import {connect} from 'react-redux'
 
@@ -6,46 +6,27 @@ type TagsType = {parent: string}[]
 type MealPlanType = {tags: TagsType}
 type PropsType = {parent: string, servingQuantity: number, tags: TagsType}
 function ServingCounter(props: PropsType) {
-// do i need the useCallback hook?
-    const servings = useCallback(
-        () => {
-            // count the number of tags with parent === parent
-            
-            
-            
 
-            let servingsArray = []
-            let i = 1
-            while (i <= props.servingQuantity - (props.tags.filter(t => t.parent === props.parent).length)) {
-                servingsArray.push(<EmptyCircle key={i} title={`unchecked ${props.parent} serving`}></EmptyCircle>)
-                i ++
-            }
-            i = 1 
-            while (i <= props.tags.filter(t => t.parent === props.parent).length) {
-                let key = props.parent + "-" + i
-                servingsArray.push(<WhiteCheckCircle key={key} title={`checked ${props.parent} serving`}></WhiteCheckCircle>)
-                i ++
-            }
-            return servingsArray 
+    const servings = () => {
+        // count the number of tags with parent === parent
+        let servingsArray = []
+        let i = 1
+        while (i <= props.servingQuantity - (props.tags.filter(t => t.parent === props.parent).length)) {
+            servingsArray.push(<EmptyCircle key={i} title={`unchecked ${props.parent} serving`}></EmptyCircle>)
+            i ++
+        }
+        i = 1 
+        while (i <= props.tags.filter(t => t.parent === props.parent).length) {
+            let key = props.parent + "-" + i
+            servingsArray.push(<WhiteCheckCircle key={key} title={`checked ${props.parent} serving`}></WhiteCheckCircle>)
+            i ++
+        }
+        return servingsArray 
+    }
 
-        },
-        [props.parent, props.tags, props.servingQuantity]
-    )
-
-    const [servingsArray, setServingsArray] = useState(servings())
-
-    useEffect(()=> {
-        // setFilledServings(props.tags.filter(t => t.parent === props.parent).length)
-        // setEmptyServings(props.servingQuantity - (props.tags.filter(t => t.parent === props.parent).length))
-        setServingsArray(servings())
-        // console.log("filledServings=", filledServings)
-    }, [servings])
-
-    
-    
     return(
         <div>
-            {servingsArray}
+            {servings()}
         </div> 
     )
 }
