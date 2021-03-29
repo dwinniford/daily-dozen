f46eaa1ed2db97256a393299bc0dcabaa28bde11
David Winniford <dwinniford@gmail.com>
2021-03-29 10:00:40 -0500

    ServingCounter counts servings of tag

--- src/components/ServingCounter.tsx
+++ src/components/ServingCounter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {EmptyCircle, WhiteCheckCircle} from '../style/icons'
 import {connect} from 'react-redux'
 
-type TagsType = {parent: string}[]
+type TagsType = {parent: string, servings: number}[]
 type MealPlanType = {tags: TagsType}
 type PropsType = {parent: string, servingQuantity: number, tags: TagsType}
 function ServingCounter(props: PropsType) {
@@ -11,12 +11,16 @@ function ServingCounter(props: PropsType) {
         // count the number of tags with parent === parent
         let servingsArray = []
         let i = 1
-        while (i <= props.servingQuantity - (props.tags.filter(t => t.parent === props.parent).length)) {
+        let totalServings = 0
+        let filteredTags = props.tags.filter(t => t.parent === props.parent)
+        filteredTags.forEach((t) => totalServings += t.servings)
+        
+        while (i <= props.servingQuantity - totalServings) {
             servingsArray.push(<EmptyCircle key={i} title={`unchecked ${props.parent} serving`}></EmptyCircle>)
             i ++
         }
         i = 1 
-        while (i <= props.tags.filter(t => t.parent === props.parent).length) {
+        while (i <= totalServings) {
             let key = props.parent + "-" + i
             servingsArray.push(<WhiteCheckCircle key={key} title={`checked ${props.parent} serving`}></WhiteCheckCircle>)
             i ++
