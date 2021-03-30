9d4733ab06187d21b5e329312ecc4300069b11af
David Winniford <dwinniford@gmail.com>
2021-03-30 12:24:16 -0500

    tests counter with 1 tag

--- src/components/ServingCounter.test.tsx
+++ src/components/ServingCounter.test.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import {unmountComponentAtNode} from 'react-dom'
 import {render, fireEvent, screen } from '@testing-library/react'
 
+import { createStore, applyMiddleware, combineReducers } from "redux"
+import thunk from 'redux-thunk'
+import mealPlan from "../redux/reducers/mealPlan"
+import builder from '../redux/reducers/builder'
+
 
 import { Provider } from 'react-redux'
 import store from '../redux/store'
@@ -13,7 +18,7 @@ let component: any = null
 beforeEach(() => {
     container = document.createElement('div')
     document.body.appendChild(container)
-    component = render(<Provider store={store} ><ServingCounter parent={"Beans"} servingQuantity={3} /></Provider>, container)
+    // component = render(<Provider store={store} ><ServingCounter parent={"Beans"} servingQuantity={3} /></Provider>, container)
 })
 
 afterEach(() => {
@@ -24,8 +29,21 @@ afterEach(() => {
 })
 
 describe("serving counter displays empty check circles for no tags", () => {
+    
     test('it renders with 3 unchecked circles', () => {
+        component = render(<Provider store={store} ><ServingCounter parent={"Beans"} servingQuantity={3} /></Provider>, container)
         expect(component.getAllByText('unchecked Beans serving')[0]).toBeInTheDocument()
         expect(component.getAllByText('unchecked Beans serving').length).toEqual(3)
     })
+    test('it shows 1 check when tag exists', () => {
+        const mockTag = {parent: "Beans", ingredient: "Black beans", servings: 1, recipeUrl: "http://ohsheglows.com/2009/06/23/back-eyed-peas-the-edible-kind/"}
+        const mockInitialState = {mealPlan: {tags: [mockTag]}}
+        const mockStore = createStore(combineReducers({builder, mealPlan}), mockInitialState, applyMiddleware(thunk));
+        component = render(<Provider store={mockStore} ><ServingCounter parent={"Beans"} servingQuantity={3} /></Provider>, container)
+        expect(component.getAllByText('unchecked Beans serving').length).toEqual(2)
+        expect(component.getByText('checked Beans serving')).toBeInTheDocument()
+    })
 }) 
+
+// how to initialize the store with a tag?
+// createStore accepts initial state as second argument
