9035b91fa8100c3a4fadea2b5e6d23d4ef8a5d69
David Winniford <dwinniford@gmail.com>
2021-04-08 14:12:56 -0500

    add text styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import {Title} from '../style/base.js'
+import {Title, MessageHolder} from '../style/base.js'
 
 export default function About() {
     return (
         <div>
             <Title>About Daily Dozen</Title>
-            "Read more about Daily Dozen at https://nutritionfacts.org/topics/daily-dozen/"
+            <MessageHolder>
+                "Read more about Daily Dozen at https://nutritionfacts.org/topics/daily-dozen/"
+            </MessageHolder>
+            
         </div>
     )
 }
