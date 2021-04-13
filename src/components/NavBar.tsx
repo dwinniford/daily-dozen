29c78786d6d20e0eec9460de226a67c5228c4b7d
David Winniford <dwinniford@gmail.com>
2021-04-13 13:21:26 -0500

    dashboard link displays if logged in

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import {NavGrid, BlackButtonLink} from '../style/base.js'
+import {connect} from 'react-redux'
 
-export default function NavBar() {
+type PropsType = {
+    loggedIn: boolean
+}
+
+function NavBar(props: PropsType) {
     return (
         <NavGrid>
             <BlackButtonLink to="/">Home</BlackButtonLink>
             <BlackButtonLink to="/about">About</BlackButtonLink>
-            <BlackButtonLink to="/dashboard">Dashboard</BlackButtonLink>
+            { props.loggedIn? <BlackButtonLink to="/dashboard">Dashboard</BlackButtonLink> : null }
             <BlackButtonLink to="/login" >Login</BlackButtonLink>
         </NavGrid>
     )
 }
+
+type StateType = {
+    user: {loggedIn: boolean}
+}
+
+const mapStateToProps = (state: StateType) => {
+    return {
+        loggedIn: state.user.loggedIn
+    }
+}
+
+export default connect(mapStateToProps)(NavBar)
