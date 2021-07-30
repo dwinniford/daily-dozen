e60e9a428b5794b448ab288a1d1b83b537e04252
David Winniford <dwinniford@gmail.com>
2021-07-30 06:59:41 -0500

    meal plan title has controlled input

--- src/containers/MealPlanTitle.tsx
+++ src/containers/MealPlanTitle.tsx
@@ -5,17 +5,21 @@ import {TitleHolder, FormInput} from '../style/dashboard'
 
 function MealPlanTitle() {
     const [edit, setEdit] = useState(false)
-    const [title, setTitle] = useState("Meal Plan")
+    const [title, setTitle] = useState("")
 
     const toggleEdit = () => {
         edit? setEdit(false) : setEdit(true)
         console.log("toggle", edit)
     }
 
+    const handleFormChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
     
     return (
         <TitleHolder>
-            {edit? <FormInput placeholder="title" /> : <Title>Meal Plan</Title>}
+            {edit? <FormInput placeholder="title" onChange={handleFormChange} value={title} /> : <Title>{title.length? title : "Meal Plan"}</Title>}
             <SuperScriptButton onClick={toggleEdit} >
                 <EditPencil title="edit title" />
             </SuperScriptButton>
