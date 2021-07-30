83512533deea09830e4f0b4bc3c06754792fa2b2
David Winniford <dwinniford@gmail.com>
2021-07-30 15:54:32 -0500

    saves title in global state

--- src/containers/MealPlanTitle.tsx
+++ src/containers/MealPlanTitle.tsx
@@ -2,8 +2,10 @@ import React, {useState} from 'react'
 import { Title, SuperScriptButton } from '../style/base'
 import {EditPencil, SaveDisk} from '../style/icons'
 import {TitleHolder, FormInput} from '../style/dashboard'
+import {connect} from 'react-redux'
 
-function MealPlanTitle() {
+type PropsType = {mealPlanTitle: string, updateTitle: Function}
+function MealPlanTitle(props: PropsType) {
     const [edit, setEdit] = useState(false)
     const [title, setTitle] = useState("")
 
@@ -18,16 +20,29 @@ function MealPlanTitle() {
 
     const saveTitle = () => {
         // set meal plan title in redux
+        props.updateTitle(title)
         toggleEdit()
     }
 
     
     return (
         <TitleHolder>
-            {edit? <FormInput placeholder="title" onChange={handleFormChange} value={title} /> : <Title>{title.length? title : "Meal Plan"}</Title>}
+            {edit? <FormInput placeholder="title" onChange={handleFormChange} value={title} /> : <Title>{props.mealPlanTitle.length? props.mealPlanTitle : "Meal Plan"}</Title>}
             {edit? <SuperScriptButton onClick={saveTitle}><SaveDisk title="save title" /></SuperScriptButton> : <SuperScriptButton onClick={toggleEdit} ><EditPencil title="edit title" /></SuperScriptButton>}
         </TitleHolder>
     )
 }
 
-export default MealPlanTitle
+type StateType = {mealPlan: {title: string}}
+const mapStateToProps = (state: StateType) => {
+    return {
+        mealPlanTitle: state.mealPlan.title
+    }
+}
+const mapDispatchToProps = (dispatch: Function) => {
+    return {
+        updateTitle: (title: string) => dispatch({type: "UPDATE_TITLE", title})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MealPlanTitle)
