4cacb02c006066f784d90a66c603cefd732af7e8
David Winniford <dwinniford@gmail.com>
2021-08-10 17:06:10 -0500

    saves meal plan

--- src/containers/MealPlan.tsx
+++ src/containers/MealPlan.tsx
@@ -4,13 +4,17 @@ import {SearchResultsGrid, DashboardBlock} from '../style/dashboard'
 import {MessageHolder, BlackButton} from '../style/base'
 import RecipeCard from '../components/RecipeCard'
 import MealPlanTitle from './MealPlanTitle'
+import {useMutation} from '@apollo/client'
+import CREATE_MEAL_PLAN from '../gql/mutations/createMealPlan'
 
 
 type RecipeProps = {label: string, source: string, ingredientLines: string[], tags: { parent: string; ingredient: string; }[], url: string, image: string;}
-type MealPlanProps = {recipes: RecipeProps[], message: string}
+type TagProps = {}
+type MealPlanProps = {recipes: RecipeProps[], message: string, tags: TagProps[], title: string}
 function MealPlan(props: MealPlanProps) {
     // const [displayed, toggleDisplayed] = useState(false)
     const [opacity, toggleOpacity] = useState(0)
+    const [createMealPlan, {data, loading, error, called}] = useMutation(CREATE_MEAL_PLAN, {errorPolicy: 'all'})
 
     useEffect(function() {
         if(props.message.length) {
@@ -22,6 +26,23 @@ function MealPlan(props: MealPlanProps) {
         }
     }, [props.message.length])
 
+    const handleSave = () => {
+        createMealPlan({variables: {
+            title: props.title,
+            recipes: props.recipes,
+            tags: props.tags
+            }
+        })
+    }
+
+
+    if(error) {
+        console.log("error", error.graphQLErrors)
+    }
+    if(called) {
+        console.log("completed, data= ", data)
+    }
+
     return (
         <DashboardBlock>
             <MealPlanTitle />
@@ -29,15 +50,17 @@ function MealPlan(props: MealPlanProps) {
             <SearchResultsGrid>
                 {props.recipes.map(r => <RecipeCard key={r.label+"-"+r.source} recipe={r} inMealPlan={true} />)}
             </SearchResultsGrid>
-            <BlackButton >Save Meal Plan</BlackButton>
+            <BlackButton onClick={handleSave} >Save Meal Plan</BlackButton>
         </ DashboardBlock>
     )
 }
 
-const mapStateToProps = (state: {mealPlan: {recipes: RecipeProps[], message: string} }) => {
+const mapStateToProps = (state: {mealPlan: {recipes: RecipeProps[], message: string, tags: TagProps[], title: string} }) => {
     return {
         recipes: state.mealPlan.recipes,
-        message: state.mealPlan.message
+        message: state.mealPlan.message,
+        tags: state.mealPlan.tags,
+        title: state.mealPlan.title
     }
 }
 
