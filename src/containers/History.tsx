23fe2d3ef4bf2612e693cd18d4a71b19192adf5d
David Winniford <dwinniford@gmail.com>
2021-08-23 07:37:22 -0500

    history uses dashboard grid style

--- src/containers/History.tsx
+++ src/containers/History.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
 import ME from '../gql/queries/me'
-import {Title} from '../style/base'
+import {Title, Subtitle} from '../style/base'
+import {DashboardGrid, DashboardBlock, SearchResultsGrid} from '../style/dashboard'
 
 type MealPlanType = {title: string, id: string}
 function History() {
     const {loading, error, data} = useQuery(ME)
 
-    if(loading) return <h1>Loading Meal Plans</h1>
+    if(loading) return <Title>Loading Meal Plans</Title>
     if(error) {
         console.log(error)
-        return <h1>Error retrieving meal plans</h1>
+        return <Title>Error retrieving meal plans</Title>
     }
     if(data) {
         return(
-            <div>
-                {data.me.mealPlans.map((m: MealPlanType) => <Title key={m.id}>{m.title}</Title>)}
-            </div>
+            <DashboardGrid>
+                <DashboardBlock>
+                    <Title>History</Title>
+                    <SearchResultsGrid>
+                        {data.me.mealPlans.map((m: MealPlanType) => <Subtitle key={m.id}>{m.title}</Subtitle>)}              
+                    </SearchResultsGrid>
+                  
+                </DashboardBlock>
+            </DashboardGrid>
         )
     }
-    return <h1>No Meal Plans</h1>
+    return <Title>No Meal Plans</Title>
 }
 
 export default History
